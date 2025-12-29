d96ef565cd5560b156f72391a7fb197544443549
Vaishal <vaishalsparikh@gmail.com>
2025-12-30 04:26:28 +0530

    Added certi logos & details

--- lib/index.ts
+++ lib/index.ts
@@ -145,3 +145,461 @@ export const REVIEWS = [
     rating: 4,
   },
 ];
+
+// CompTIA Certification Products
+
+export const CompTIA = [
+  {
+    name: "CompTIA A+ (220-1201 / 220-1202)",
+    category: "CompTIA",
+    actualPrice: 19095,
+    ourPrice: 10120,
+    image: "/images/product/comptia A-new.webp",
+  },
+  {
+    name: "CompTIA Data+ (DA0-001)",
+    category: "CompTIA",
+    actualPrice: 18376,
+    ourPrice: 10120,
+    image: "/images/product/compTIA-Data-plus.webp",
+  },
+  {
+    name: "CompTIA Network+ (N10-009)",
+    category: "CompTIA",
+    actualPrice: 28026,
+    ourPrice: 14802,
+    image: "/images/product/comptia-Network-new.webp",
+  },
+  {
+    name: "CompTIA Cloud+ (CV0-004)",
+    category: "CompTIA",
+    actualPrice: 28026,
+    ourPrice: 14802,
+    image: "/images/product/comptia-Cloud-new.webp",
+  },
+  {
+    name: "CompTIA Linux+ (XK0-005)",
+    category: "CompTIA",
+    actualPrice: 28026,
+    ourPrice: 14802,
+    image: "/images/product/comptia-Linux-new.webp",
+  },
+  {
+    name: "CompTIA Server+ (SK0-005)",
+    category: "CompTIA",
+    actualPrice: 28026,
+    ourPrice: 14802,
+    image: "/images/product/comptia-Server-new.webp",
+  },
+  {
+    name: "CompTIA DataSys+ (DS0-001)",
+    category: "CompTIA",
+    actualPrice: 28026,
+    ourPrice: 14802,
+    image: "/images/product/Data-Sys-plus.webp",
+  },
+  {
+    name: "CompTIA Project+ (PK0-005)",
+    category: "CompTIA",
+    actualPrice: 28026,
+    ourPrice: 14802,
+    image: "/images/product/comptia-Project-new.webp",
+  },
+  {
+    name: "CompTIA Security+ (SY0-701)",
+    category: "CompTIA",
+    actualPrice: 30592,
+    ourPrice: 16086,
+    image: "/images/product/comptia-Security-new.webp",
+  },
+  {
+    name: "CompTIA CySA+ (CS0-003)",
+    category: "CompTIA",
+    actualPrice: 30592,
+    ourPrice: 16086,
+    image: "/images/product/comptia-CySA-new.webp",
+  },
+  {
+    name: "CompTIA PenTest+ (PT0-002)",
+    category: "CompTIA",
+    actualPrice: 30592,
+    ourPrice: 16086,
+    image: "/images/product/comptia-PenTest-new.webp",
+  },
+  {
+    name: "CompTIA SecurityX (CAS-005)",
+    category: "CompTIA",
+    actualPrice: 37984,
+    ourPrice: 22420,
+    image: "/images/product/SecurityX.webp",
+  },
+  {
+    name: "CompTIA CloudNetX (CNX-001)",
+    category: "CompTIA",
+    actualPrice: 37984,
+    ourPrice: 22420,
+    image: "/images/product/CloudNetX.webp",
+  },
+  {
+    name: "CompTIA DataX (DY0-001)",
+    category: "CompTIA",
+    actualPrice: 37984,
+    ourPrice: 22420,
+    image: "/images/product/DataX.webp",
+  },
+];
+
+export const ORACLE = [
+  {
+    name: "Oracle Certification Exam Vouchers",
+    category: "Oracle",
+    actualPrice: 25212,
+    ourPrice: 14750,
+    image: "/images/product/Oracle-Image.webp",
+  },
+];
+
+export const AWS = [
+  {
+    name: "AWS Certified Cloud Practitioner (CLF-C02)",
+    category: "AWS",
+    actualPrice: 10266,
+    ourPrice: 8260,
+    image: "/images/product/fund-Cloud-Practioner-new.webp",
+  },
+  {
+    name: "AWS Certified AI Practitioner (AIF-C01)",
+    category: "AWS",
+    actualPrice: 10266,
+    ourPrice: 8260,
+    image: "/images/product/AI-Practitioner.webp",
+  },
+  {
+    name: "AWS Certified SysOps Administrator – Associate (SOA-C02)",
+    category: "AWS",
+    actualPrice: 15399,
+    ourPrice: 12390,
+    image: "/images/product/Associate-SysOps administrator-new.webp",
+  },
+  {
+    name: "AWS Certified Developer – Associate (DVA-C02)",
+    category: "AWS",
+    actualPrice: 15399,
+    ourPrice: 12390,
+    image: "/images/product/Associate-Developer-new.webp",
+  },
+  {
+    name: "AWS Certified Solutions Architect – Associate (SAA-C03)",
+    category: "AWS",
+    actualPrice: 15399,
+    ourPrice: 12390,
+    image: "/images/product/Associate-Solution Architect-new.webp",
+  },
+  {
+    name: "AWS Certified Data Engineer – Associate (DEA-C01)",
+    category: "AWS",
+    actualPrice: 15399,
+    ourPrice: 12390,
+    image: "/images/product/AWS-Certified-Data-Engineer-Associate.webp",
+  },
+  {
+    name: "AWS Certified Machine Learning Engineer – Associate (MLA-C01)",
+    category: "AWS",
+    actualPrice: 15399,
+    ourPrice: 12390,
+    image: "/images/product/AWS-Certified-Machine-Learning-Engineer.webp",
+  },
+  {
+    name: "AWS Certified Solutions Architect – Professional",
+    category: "AWS",
+    actualPrice: 30798,
+    ourPrice: 24780,
+    image: "/images/product/Professional-Solution-Architect-new.webp",
+  },
+  {
+    name: "AWS Certified DevOps Engineer (DOP-C02)",
+    category: "AWS",
+    actualPrice: 30798,
+    ourPrice: 24780,
+    image: "/images/product/Professional-DevOps-Engineer-new1.webp",
+  },
+  {
+    name: "AWS Certified Advanced Networking",
+    category: "AWS",
+    actualPrice: 30798,
+    ourPrice: 24780,
+    image: "/images/product/Specialty-Advance Networking-new.webp",
+  },
+  {
+    name: "AWS Certified Machine Learning",
+    category: "AWS",
+    actualPrice: 30798,
+    ourPrice: 24780,
+    image: "/images/product/Specialty-Machine Learning-new.webp",
+  },
+  {
+    name: "AWS Certified Security",
+    category: "AWS",
+    actualPrice: 30798,
+    ourPrice: 24780,
+    image: "/images/product/Specialty-Security Speciality-new.webp",
+  },
+];
+
+export const VMWARE = [
+  {
+    name: "Vmware Certified Advanced Professional",
+    category: "VMware",
+    actualPrice: 24780,
+    ourPrice: 23010,
+    image: "/images/product/VMware-Certified-Advanced-Professional-new.webp",
+  },
+  {
+    name: "Vmware Certified Professional",
+    category: "VMware",
+    actualPrice: 24780,
+    ourPrice: 23010,
+    image: "/images/product/VMware Certified Professional-new.webp",
+  },
+];
+
+export const Microsoft = [
+  {
+    name: "Microsoft Fundamentals",
+    category: "Microsoft",
+    actualPrice: 4355,
+    ourPrice: 3100,
+    image: "/images/product/ms-singal-new.webp",
+  },
+  {
+    name: "Microsoft Single Shot",
+    category: "Microsoft",
+    actualPrice: 5741,
+    ourPrice: 4020,
+    image: "/images/product/ms-singal-new.webp",
+  },
+];
+
+export const ISTQB = [
+  {
+    name: "ISTQB Foundation Level",
+    category: "ISTQB",
+    actualPrice: 10165,
+    ourPrice: 6372,
+    image: "/images/product/istqb-fundation-new.webp",
+  },
+  {
+    name: "ISTQB Advanced Test Manager",
+    category: "ISTQB",
+    actualPrice: 18287,
+    ourPrice: 14160,
+    image: "/images/product/istqb-advanced-test-manager-new.webp",
+  },
+  {
+    name: "ISTQB Advanced Level Test Automation Engineer",
+    category: "ISTQB",
+    actualPrice: 18287,
+    ourPrice: 14160,
+    image: "/images/product/istqb-advanced-automation-engineer-new.webp",
+  },
+  {
+    name: "ISTQB Advanced Level Test Analyst",
+    category: "ISTQB",
+    actualPrice: 18287,
+    ourPrice: 14160,
+    image: "/images/product/istqb-advanced-analyst-new.webp",
+  },
+  {
+    name: "ISTQB Advanced Level Technical Test Analyst",
+    category: "ISTQB",
+    actualPrice: 18287,
+    ourPrice: 14160,
+    image: "/images/product/istqb-advanced-test-analyst-new.webp",
+  },
+];
+
+export const KUBERNETES = [
+  {
+    name: "Certified Kubernetes Application Developer (CKAD)",
+    category: "Kubernetes",
+    actualPrice: 45588,
+    ourPrice: 28320,
+    image:
+      "/images/product/Certified-Kubernetes-Application-Developer-small-new.webp",
+  },
+  {
+    name: "Certified Kubernetes Administrator (CKA)",
+    category: "Kubernetes",
+    actualPrice: 45588,
+    ourPrice: 28320,
+    image: "/images/product/Certified-Kubernetes-Vouchers-small-new.webp",
+  },
+];
+
+export const SALESFORCE = [
+  {
+    name: "Salesforce Exam Vouchers",
+    category: "Salesforce",
+    actualPrice: 19824,
+    ourPrice: 16520,
+    image: "/images/product/Salesforce-Exam-Voucher-new1.png",
+  },
+];
+
+export const ISACA = [
+  {
+    name: "Certified Information Security Manager (CISM)",
+    category: "ISACA",
+    actualPrice: 80712,
+    ourPrice: 61360,
+    image: "/images/product/certified-information-security-manager-new.webp",
+  },
+  {
+    name: "Certified in Risk and Information Systems Control (CRISC)",
+    category: "ISACA",
+    actualPrice: 80712,
+    ourPrice: 61360,
+    image:
+      "/images/product/certified-in-risk-and-information-systems-control-new.webp",
+  },
+  {
+    name: "Certified in the Governance of Enterprise IT (CGEIT)",
+    category: "ISACA",
+    actualPrice: 80712,
+    ourPrice: 61360,
+    image:
+      "/images/product/certified-in-the-governance-of-enterprise-it-new.webp",
+  },
+  {
+    name: "Certified Information Systems Auditor (CISA)",
+    category: "ISACA",
+    actualPrice: 80712,
+    ourPrice: 61360,
+    image: "/images/product/Certified-information-systems-auditor-new.webp",
+  },
+  {
+    name: "COBIT® 2019 Foundation Certification",
+    category: "ISACA",
+    actualPrice: 18585,
+    ourPrice: 18054,
+    image: "/images/product/COBIT-2019-foundation-new.webp",
+  },
+];
+
+export const ECCouncil = [
+  {
+    name: "CEH v13 Exam Voucher",
+    category: "EC-Council",
+    actualPrice: 42480,
+    ourPrice: 34220,
+    image: "/images/product/ceh.webp",
+  },
+  {
+    name: "CEH Practical",
+    category: "EC-Council",
+    actualPrice: 21240,
+    ourPrice: 17700,
+    image: "/images/product/ceh-ethecal-hacker.webp",
+  },
+  {
+    name: "Computer Hacking Forensic Investigator (CHFI)",
+    category: "EC-Council",
+    actualPrice: 41300,
+    ourPrice: 35400,
+    image: "/images/product/ceh-hacking.webp",
+  },
+];
+
+export const CISCO = [
+  {
+    name: "Associate CCNA/CCNP Concentration Specialist Exam",
+    category: "Cisco",
+    actualPrice: 31860,
+    ourPrice: 27140,
+    image: "/images/product/CISCO-LOGO.webp",
+  },
+  {
+    name: "CCNP Technology Core Exam",
+    category: "Cisco",
+    actualPrice: 42480,
+    ourPrice: 36108,
+    image: "/images/product/CISCO-LOGO.webp",
+  },
+];
+
+export const PEGA = [
+  {
+    name: "PEGA Certification Exam Voucher",
+    category: "PEGA",
+    actualPrice: 19470,
+    ourPrice: 16520,
+    image: "/images/product/PEGA-exam-voucher.webp",
+  },
+];
+
+export const SPLUNK = [
+  {
+    name: "Splunk Certification Exam Voucher",
+    category: "Splunk",
+    actualPrice: 14632,
+    ourPrice: 10620,
+    image: "/images/product/splunk-exam-vaoucher.webp",
+  },
+];
+
+export const DELLEMC = [
+  {
+    name: "Dell EMC Certification Exam Voucher",
+    category: "Dell EMC",
+    actualPrice: 22599,
+    ourPrice: 16520,
+    image: "/images/product/Dell-EMC-Certification-Exam-vucher.webp",
+  },
+];
+
+export const SAS = [
+  {
+    name: "SAS Certification Exam Vouchers",
+    category: "SAS",
+    actualPrice: 17936,
+    ourPrice: 14750,
+    image: "/images/product/sas-certification-exam-vouchers.webp",
+  },
+];
+
+export const Fortinet = [
+  {
+    name: "Fortinet Certification Exam Voucher",
+    category: "Fortinet",
+    actualPrice: 21000,
+    ourPrice: 17853,
+    image: "/images/product/fortinet-card.webp",
+  },
+];
+
+export const Checkpoint = [
+  {
+    name: "Checkpoint Certification Exam Voucher",
+    category: "Checkpoint",
+    actualPrice: 31860,
+    ourPrice: 18000,
+    image: "/images/product/checkpoint-card.webp",
+  },
+];
+
+export const Juniper = [
+  {
+    name: "Juniper Certification Exam Voucher (Selected Exams)",
+    category: "Juniper",
+    actualPrice: 21000,
+    ourPrice: 7670,
+    image: "/images/product/juniper-card.webp",
+  },
+  {
+    name: "Juniper Certification Exam Voucher (All Exams)",
+    category: "Juniper",
+    actualPrice: 42000,
+    ourPrice: 14000,
+    image: "/images/product/juniper-card.webp",
+  },
+];
